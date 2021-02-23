1f550dcc2e0ce5db28dc0547f749226786ed2010
Menno de Graaf <menno@motionundercontrol.nl>
2021-02-23 11:33:13 +0100

    Send 32-bit message counter and timestamp

--- can-sender/main.cpp
+++ can-sender/main.cpp
@@ -1,8 +1,14 @@
 #include "mbed.h"
 
-DigitalOut led1(PC_13);     // Onboard LED
-CAN can(PB_8, PB_9);        // Onboard CAN channel connected to TI SN65HVD230 transceiver
-Ticker t;                   // Ticker with microsecond resolution
+DigitalOut led1(PC_13);         // Onboard LED
+CAN can(PB_8, PB_9, 250000);    // Onboard CAN channel connected to TI SN65HVD230 transceiver
+Ticker ticker;                  // Ticker with microsecond resolution
+Timer timer;                    // Timer with microsecond resolution
+
+// Global data
+char can_data[8];
+uint32_t counter = 0;
+uint32_t t_send;
 
 // Serial console output is sent by default to UART2 (PA_2, PA_3)
 //BufferedSerial serial(PA_2, PA_3, 115200);
@@ -15,15 +21,26 @@ Ticker t;                   // Ticker with microsecond resolution
 // Periodically called
 void onTick(void) {
     led1 = !led1;
-    if (led1) can.write(CANMessage(0x80));
-    else can.write(CANMessage(0x81));
+    counter++;
+    // Construct a message with counter and send timestamp
+    can_data[0] = (counter >> 24) & 0xFF;
+    can_data[1] = (counter >> 16) & 0xFF;
+    can_data[2] = (counter >> 8) & 0xFF;
+    can_data[3] = counter & 0xFF;
+    t_send = timer.elapsed_time().count();
+    can_data[4] = (t_send >> 24) & 0xFF;
+    can_data[5] = (t_send >> 16) & 0xFF;
+    can_data[6] = (t_send >> 8) & 0xFF;
+    can_data[7] = t_send & 0xFF;
+    can.write(CANMessage(0x80, can_data, 8));
 }
 
 int main()
 {
     printf("This is a CAN send example on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
-    t.attach(&onTick,200ms);      // periodically call the onTick function
+    timer.start();
+    ticker.attach(&onTick,100ms);      // periodically call the onTick function
 
     while (true)
     {
